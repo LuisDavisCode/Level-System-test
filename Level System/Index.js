8e0cf088a9570e5358e03ec6ad60c7904ee3632c
LuisDavisCode <luis1930poti@gmail.com>
2023-11-28 20:30:58 -0300

    end

--- Level System/Index.js
+++ Level System/Index.js
@@ -2,3 +2,46 @@
 
 let name = "Aldrich"
 let Xp = 0
+
+switch (true){
+    
+    case Xp <= 1000:
+    console.log("The hero " + name + " are at Iron Rank")
+    break
+    
+    case Xp >= 1001 && Xp <= 2000:
+    console.log("The hero " + name + " are at Bronze Rank")
+    break
+
+    case Xp >= 2001 && Xp <= 5000:
+    console.log("The hero " + name + " are at Silver Rank")
+    break
+
+    case Xp >= 5001 && Xp <= 7000:
+    console.log("The hero " + name + " are at Gold Rank")
+    break
+
+    case Xp >= 7001 && Xp <= 8000:
+    console.log("The hero " + name + " are at Platinum Rank")
+    break
+
+    case Xp >= 8001 && Xp <= 9000 :
+    console.log("The hero " + name + " are at Diamond Rank")
+    break
+
+    case Xp >= 9001 && Xp <= 10000 :
+    console.log("The hero " + name + " are at Imortal Rank")
+    break
+
+    case Xp >= 10001 :
+    console.log("The hero " + name + " are at God Rank")
+    break  
+}   
+
+
+
+
+
+
+
+
